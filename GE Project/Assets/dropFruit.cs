4449c24071d4dcdd3b8f126589109143a6eaaea3
Daniel Di Nardi <c18487682@mytudublin.ie>
2021-12-09 11:45:33 +0000

    Created a corner that spews figs by mistake gotta fix that

---
+++ GE Project/Assets/dropFruit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropFruit : MonoBehaviour
+{
+    public GameObject fig;
+
+    System.Collections.IEnumerator Drop()
+    {
+        while(true)
+        {
+            Debug.Log("Here");
+            Instantiate(fig, new Vector3(-1.44f, 4.2f, 0.78f), transform.rotation);
+            fig.transform.parent = transform;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void OnEnable()
+    {
+        StartCoroutine(Drop());
+    }
+}
