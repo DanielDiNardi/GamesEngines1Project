cd1fbc3438c36a9c1cda4526bd6b49f86d071dcd
Daniel Di Nardi <c18487682@mytudublin.ie>
2021-12-10 17:56:40 +0000

    Added raycasting to check if water was under spawn point and merged spawn scripts into one spawner script

---
+++ GE Project/Assets/spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    GameObject[] objects;
+    public GameObject monkey;
+    public GameObject marten;
+    public GameObject tree;
+    public int[] populations = new int[] {100, 10, 50};
+    public float spawnWidth = 246f;
+    public float spawnHeight = 246f;
+
+    System.Collections.IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(1);
+        RaycastHit hit;
+
+        for(int i = 0; i < objects.Length; i++){
+            for(int j = 0; j < populations[i]; j++){
+                Vector3 spawnPoint = new Vector3(Random.Range(10f, spawnWidth), transform.position.y, Random.Range(10f, spawnHeight));
+                if(Physics.Raycast(spawnPoint, transform.TransformDirection(Vector3.down), out hit)){
+                    if(hit.distance < 4.43f){
+                        Instantiate(objects[i], spawnPoint, transform.rotation);
+                    }
+                }
+            }
+        }
+
+    }
+
+    void CallCoroutine()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    void Start(){
+
+        objects = new GameObject[] {monkey, marten, tree};
+
+        CallCoroutine();
+
+    }
+}
